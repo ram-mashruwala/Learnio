fda10fe2319a4a28bfcdf9e659e3aa23c8de5b0b
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-08-09 14:54:58 -0700

    Added a shell context

--- learnio.py
+++ learnio.py
@@ -1,1 +1,9 @@
 from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from app.models import User
+from app.extensions import db
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"sa": sa, "orm": orm, "db": db, "User": User}
