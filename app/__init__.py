7ad48f019f8a942431d9cff4da6a5a78e28c85f6
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-08-09 14:50:10 -0700

    Added extensions.py file and added config file
    Added extensions.py file to group database and migrations objects
    togeather and to also group other objects in the future as well
    
    Added a configuration file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from app.extensions import db, migrate
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app=app)
+migrate.init_app(app=app, db=db)
 
 from app.api import api_bp
 from app.web import web_bp
-
-app = Flask(__name__)
-
 app.register_blueprint(web_bp)
 app.register_blueprint(api_bp)
