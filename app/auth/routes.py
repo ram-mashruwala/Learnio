a3adf52a7e799c2b14ed7e57922cb446dacf5b02
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-08-10 16:07:27 -0700

    Added POST method to register and login endpoints

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,12 +2,12 @@ from flask import render_template
 from . import auth_bp
 from .form import LoginForm, RegisterForm
 
-@auth_bp.route("/login")
+@auth_bp.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     return render_template("login.html", form=form)
 
-@auth_bp.route("/register")
+@auth_bp.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
     return render_template("register.html", form=form)
