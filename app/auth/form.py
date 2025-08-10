1293c9c549ac8f26018892a5a260d0b1cb5208ad
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-08-09 19:11:10 -0700

    Added Registration form in form.py
    Registration form has these fields:
    
    Username - A string field that rejects usernames that already exist
    Email - A string field that needs to be in an email format and needs to
    not already exist
    password - A password field
    repeat password - A password field that need to be the same as password
    terms and conditions - A boolean field that needs to be accepted
    register button - A submit field that registers the user

--- app/auth/form.py
+++ app/auth/form.py
@@ -10,3 +10,21 @@ class LoginForm(FlaskForm):
     password = PasswordField(label="Password", validators=[DataRequired()])
     remember_me = BooleanField(label="Remember Me")
     submit_button = SubmitField(label="Submit")
+
+class RegisterForm(FlaskForm):
+    username = StringField(label="Username", validators=[DataRequired(), Length(30)])
+    email = EmailField(label="Email", validators=[DataRequired(), Email(message="Must be a valid Email Address")])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    repeat_password = PasswordField(label="Repeat Password", validators=[DataRequired(), EqualTo("password", message="Passwords must Match!")])
+    terms_and_conditions = BooleanField(label="Terms and Conditions", validators=[DataRequired()])
+    register_button = SubmitField(label="Register")
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user:
+            raise ValidationError("Username is already taken!!")
+
+    def validate_email(self, email):
+        email = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if email:
+            raise ValidationError("Email Already Taken!")
