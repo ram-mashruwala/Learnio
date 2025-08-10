fa09a70ef678f829e570f26dd473c81035dd7cd7
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-08-10 16:05:36 -0700

    Put each arugument of Blueprint constructor in different line

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
 
-auth_bp = Blueprint("auth", __name__, url_prefix="/auth", template_folder="templates", static_folder="static")
+auth_bp = Blueprint("auth", __name__, 
+                    url_prefix="/auth", 
+                    template_folder="templates", 
+                    static_folder="static")
 
 from . import routes
