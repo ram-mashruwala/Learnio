adf1e87e0ee5bbd24d745f7aebbed72275ec7c0c
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-08-11 17:18:48 -0700

    User now needs to be logged in to access index endpoint

--- app/web/routes.py
+++ app/web/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template
+from flask import render_template, url_for
+from flask_login import login_required
 from . import web_bp
 
 @web_bp.route('/index')
 @web_bp.route('/')
+@login_required
 def index():
     return render_template("index.html")
