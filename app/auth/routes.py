4114e3f004b3cc49ae7d3745d4a3dbd20570b796
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-08-11 17:06:07 -0700

    Added Login support in "/auth/login" endpoint

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,26 @@
-from flask import render_template
+from urllib.parse import urlsplit
+from flask_login import current_user, login_required, login_user, logout_user
+from app.models import User
+import sqlalchemy as sa
+from app.extensions import db
+from flask import flash, redirect, render_template, url_for, request
 from . import auth_bp
 from .form import LoginForm, RegisterForm
 
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        select_stmt = sa.select(User).where(User.username == form.username.data)
+        user = db.session.scalar(select_stmt)
+        if not user or user.check_password(user.password_hash):
+            flash("Invalid Username or Password")
+            return redirect(url_for("auth.login"))
+        login_user(user)
+        next = request.args.get("next")
+        if not next or urlsplit(next).netloc != "" or next == "/":
+            return redirect(url_for("web.index"))
+        return redirect(next)
     return render_template("login.html", form=form)
 
 @auth_bp.route("/register", methods=["GET", "POST"])
